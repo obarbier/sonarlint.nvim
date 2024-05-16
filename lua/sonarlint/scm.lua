a8b529eeadfd91499d50015981502f2d8cf47a0a
Patrice FERLET <metal3d@gmail.com>
2024-05-16 19:52:28 +0000

    Fix an error if workspace_folders is nil

--- lua/sonarlint/scm.lua
+++ lua/sonarlint/scm.lua
@@ -12,7 +12,11 @@ function M.check_git_branch_and_notify_lsp(args)
    end
 
    local clients = vim.lsp.get_active_clients({ name = "sonarlint.nvim", bufnr = bufnr })
-   for i, client in ipairs(clients) do
+   for _, client in ipairs(clients) do
+      -- ensure that client.workspace_folders is not nil
+      if not client.workspace_folders then
+         goto continue
+      end
       if M.last_heads_by_lsp_client[client.id] ~= gitsigns_status.head then
          client.notify("sonarlint/didLocalBranchNameChange", {
             folderUri = client.workspace_folders[1].uri,
@@ -21,10 +25,11 @@ function M.check_git_branch_and_notify_lsp(args)
 
          M.last_heads_by_lsp_client[client.id] = gitsigns_status.head
       end
+      ::continue::
    end
 end
 
-function M.is_ignored_by_scm(err, file_uri)
+function M.is_ignored_by_scm(_, file_uri)
    local uri = type(file_uri) == "table" and file_uri[1] or file_uri
    local bufnr = vim.uri_to_bufnr(uri)
    local ok, gitsigns_status = pcall(function()
